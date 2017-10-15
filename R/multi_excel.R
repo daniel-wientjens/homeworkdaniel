5bc71182f5cd952301b188dd92d84d3ce0f2c929
Daniel Wientjens <Daniel@Daniels-MacBook-Pro.local>
2017-10-15 16:43:06 +0200

    function draw_names is added here

---
+++ R/multi_excel.R
@@ -0,0 +1,20 @@
+#' This is the function to read multiple excel sheets into a dataframe
+#'
+#' @param file_name
+#' @import readxl
+#' @import assertthat
+#'
+#' @return the list of dataframes coming from the excel tabs
+#' @export
+#'
+#' @examples
+#' multi_excel("Werkzaamheden Tom")
+#'
+multi_excel <- function(file_name){
+  assert_that(is.dir(dirname(file_name)))
+  assert_that(is.readable(file_name))
+  assert_that(has_extension(file_name,"xlsx"))
+
+  sheets <- lapply(excel_sheets(file_name), read_excel, path=file_name)
+  return(sheets)
+}
